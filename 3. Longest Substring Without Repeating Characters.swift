6a9f7c272c6c7a556242ed5fed410b6d7dc70bc6
Mohammad Faani <rfaani@gmail.com>
2021-06-30 08:29:47 -0400

    Update 3. Longest Substring Without Repeating Characters.swift
    added a working solution.

--- 3. Longest Substring Without Repeating Characters.swift
+++ 3. Longest Substring Without Repeating Characters.swift
@@ -1,4 +1,50 @@
+// 844 ms, faster than 13.13% of Swift online submissions for Longest Substring Without Repeating Characters.
+// Memory Usage: 14.9 MB, less than 36.72% 
+
 class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        guard s != "" else { return 0 }
+        // exceptions: "" should return 0        
+        // brute force
+        
+        var dict: [Character: Int] = [:]
+        var strings: [Character] = []
+        
+        for char in s {
+            strings.append(char) 
+        }
+        
+        var res = 0
+        for i in 0..<strings.count {
+            // 1. for every iteration, create a set and check that set upon every increment...
+            var s = Set<Character>()
+            var current = strings[i]
+            var max = 0
+            for j in i..<strings.count {
+                defer {
+                    s.insert(strings[j])
+                }
+                if s.contains(strings[j]) == false {                    
+                    max += 1
+                    if max > res {
+                        res = max
+                    }
+                } else {
+                    break   
+                }                
+            }
+        }
+        return res
+    }
+}
+
+// the checking last index won't work, because we might have a longer substring that uses the character at that later index...
+
+// don't go with a simple test case. Go with the most simplest test case ever. like only 1 or 2 characters. 
+// don't think of of 4-5 as simple. That's complex :D
+
+// ------------------------
+class SolutionNotWorking {
     func lengthOfLongestSubstring(_ s: String) -> Int {
         // exceptions: "" should return 0        
         // brute force
