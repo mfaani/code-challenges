362a8f6eb0bb78268f3fb71581d2ac5ead55bf4d
Mohammad Faani <rfaani@gmail.com>
2021-09-29 20:46:58 -0400

    Update 559. Maximum Depth of N-ary Tree.swift

--- Tree/559. Maximum Depth of N-ary Tree.swift
+++ Tree/559. Maximum Depth of N-ary Tree.swift
@@ -11,7 +11,7 @@
  * }
  */
 
-class Solution {
+class Solution2 {
     func maxDepth(_ root: Node?) -> Int {
         // start with a depth of 0
         
@@ -46,3 +46,34 @@ class Solution {
 
 */
 
+
+class Solution {
+    var res = 0
+    func maxDepth(_ root: Node?) -> Int {
+        guard let node = root else { return 0}
+        traverse(node, 1) 
+        return res
+    } 
+    
+    func traverse(_ node: Node, _ dep: Int) {
+        guard node.children.isEmpty == false else { 
+            res = max(res, 1) // exception case of having 1 item. 
+            return 
+        }
+        res = max(res, dep + 1)
+        for child in node.children { 
+            traverse(child, dep + 1)
+        }
+    }    
+}
+
+
+
+// LESSONS: in recursions, never call your function again before your next iteration/level/mutation. you should only call yourself AFTER some mutation/leveling up
+// Otherwise it would be an endless loop. 
+// avoid having external param names for functions, just use `_`
+// Exceptions requires hacks/tweaks and it's fine! Exceptions are usually for items that happen before you can form a base-class...
+// Anytime your loop don't end it should imply your leveing down is incorrect. 
+// if there aren't any queues then it's DFS, 
+// DFS: is for loop on all children and recursive calling of the func on the new child...
+// BFS: is enqueue node, do a WHILE loop on queue + for loop on what's in the queue, remove what's at the tip of the queue. Add each child (from the block...remember the node has just been removed) into queue. Something like that...
