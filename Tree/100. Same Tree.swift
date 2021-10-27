afcacdd2317d3d7844cff16c39089154d35e34b8
Mohammad Faani <rfaani@gmail.com>
2021-10-27 12:01:43 -0400

    Update 100. Same Tree.swift
    added more elegant solution

--- Tree/100. Same Tree.swift
+++ Tree/100. Same Tree.swift
@@ -1,6 +1,23 @@
 // https://leetcode.com/problems/same-tree/solution/
 
-class Solution {
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+
+Checks upon a normal traversal...
+class Solution2 {
     var doesMatch = true
     func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
         compare(p,q)
@@ -12,7 +29,6 @@ class Solution {
             doesMatch = false
             return
         }
-        // no more comparisons from below _this empty_ node, but other nodes might still continue
         if p == nil { return }
         guard p?.left?.val == q?.left?.val else { 
             doesMatch = false
@@ -24,6 +40,23 @@ class Solution {
             return
         }
         compare(p?.right, q?.right)
+    }
+}
+
+// your logic shouldn't be tried to which node you tried **first**
+
+
+// MORE ELEGANT SOLUTION
+// You exit early — if values don't match.
+// Beyond that, you keep traversing / checking your left and right. 
+// each call either exits early or has to traverse more
+class Solution {
+    var doesMatch = true
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if p == nil && q == nil { return true }
+        if (p == nil || q == nil ) { return false }
+        if (p?.val != q?.val) { return false }
+        return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
     }
 }
 
