df3ca1e058b033131c3c0e66015d089cdbb437a4
Mohammad Faani <rfaani@gmail.com>
2021-12-17 17:59:39 -0500

    Update 70. Climbing Stairs.swift

--- Dynamic Programming/70. Climbing Stairs.swift
+++ Dynamic Programming/70. Climbing Stairs.swift
@@ -28,14 +28,17 @@ struct Tuple: Hashable {
     let jump: Int
 }
 
-// LESSONS:
-// MISTAKES: 
-// - I initially made a mistake. i.e. with a rem == 0 I did both jumps and then increased the count. This results in double counting. 
-// - I need to return otherwise I won't be able to memoize. 
+/* LESSONS:
+MISTAKES: 
+- I initially made a mistake. i.e. with a rem == 0 I did both jumps and then increased the count. This results in double counting. 
+- I need to return otherwise I won't be able to memoize. 
+- You must almost never want to just return a `Bool` for if a path exists. 9/10 a path does exist. 
+It's more about the number of paths that exist or the length of that path. So ditch the boolean approach :| 
 
-// - PRO-TIP: For memoization you need to return. Can't have void functions 
+- PRO-TIP: For memoization you need to return. Can't have void functions 
+- PRO-TIP: How to read from your memoization table: Don't try reading. Try seeing if you can just return or exit or something. 
+Towards the very end you should look into the cache. And at the end inject into your cache. 
 
-// QUESTION: 
-// You are climbing a staircase. It takes n steps to reach the top.
-
-// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+QUESTION: 
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
